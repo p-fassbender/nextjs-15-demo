3939a4f743117309d1c194320f0f7bb2a08c8a0b
Preston Fassbender <fassbenderp0551@gmail.com>
2025-04-10 13:34:50 -0500

    routing metadata

---
+++ routing-demo/src/app/(main)/counter/page.js
@@ -0,0 +1,11 @@
+import { Counter } from "./counter"
+
+export const metadata = {
+    title: 'counter'
+}
+
+export default function CounterPage() {
+    return (
+        <Counter />
+    )
+}
--- routing-demo/src/app/(main)/products/[productId]/page.js
+++ routing-demo/src/app/(main)/products/[productId]/page.js
@@ -1,3 +1,10 @@
+export const generateMetadata = async ({ params }) => {
+    const id = (await params).productId
+    return {
+        title: `product ${id}`
+    }
+}
+
 export default async function ProductDetails({ params }) {
     const productId = (await params).productId
     return <h1>Details about product {productId}</h1>
