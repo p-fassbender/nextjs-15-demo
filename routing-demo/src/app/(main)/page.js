738a21c5d3dc90e0ad264c1f8c243f95eaafa8d1
Preston Fassbender <fassbenderp0551@gmail.com>
2025-04-11 09:54:57 -0500

    laptop test, added links to homepage for all routes

--- routing-demo/src/app/(main)/page.js
+++ routing-demo/src/app/(main)/page.js
@@ -4,9 +4,15 @@ export default function Home() {
     return (
         <>
             <h1>Welcome Home!</h1>
-            <Link href={'/blog'}>Blog</Link>
-            <Link href={'/products'}>Products</Link>
-            <Link href={'/articles/breaking-news-123?lang=en'}>Read article</Link>
+            <Link href={'/profile'}>profile</Link> <br/>
+            <Link href={'/blog'}>blog</Link> <br/>
+            <Link href={'/about'}>about</Link> <br/>
+            <Link href={'/products'}>products</Link><br/>
+            <Link href={'/order-product'}>order product</Link><br/>
+            <Link href={'/docs'}>docs</Link><br/>
+            <Link href={'/dashboard'}>dashboard</Link><br/>
+            <Link href={'/counter'}>counter</Link><br/>
+            <Link href={'/articles/breaking-news-123?lang=en'}>read article</Link><br/>
         </>
     )
 }
