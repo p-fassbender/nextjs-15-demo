a83a99cea5b4c108757f83919bbfb542d770b280
Preston Fassbender <fassbenderp0551@gmail.com>
2025-04-16 14:01:22 -0500

    parallel routes don't work with catch all not found page so i changed it and it seems to work with a global notfound now

--- routing-demo/src/app/(main)/complex-dashboard/@notifications/page.js
+++ routing-demo/src/app/(main)/complex-dashboard/@notifications/page.js
@@ -1,7 +1,13 @@
 import { Card } from "@/components/card";
+import Link from "next/link";
 
 export default function Notifications() {
-    return (
-        <Card>Notifications</Card>
-    )
+  return (
+    <Card>
+      <div>Notifications</div>
+      <div>
+        <Link href="/complex-dashboard/archived">Archived</Link>
+      </div>
+    </Card>
+  );
 }
