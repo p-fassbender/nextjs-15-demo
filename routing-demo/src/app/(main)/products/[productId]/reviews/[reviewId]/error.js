8b6575bd2db9c6685373836c19923205762e4189
Preston Fassbender <fassbenderp0551@gmail.com>
2025-04-16 09:48:07 -0500

    recovering from erros

--- routing-demo/src/app/(main)/products/[productId]/reviews/[reviewId]/error.js
+++ routing-demo/src/app/(main)/products/[productId]/reviews/[reviewId]/error.js
@@ -1,7 +1,22 @@
 'use client'
 
-export default function ErrorBoundary({ error }) {
+import { useRouter } from "next/navigation"
+import { startTransition } from "react"
+
+export default function ErrorBoundary({ error, reset }) {
+
+    const router = useRouter()
+    const reload = () => {
+        startTransition(() => {
+            router.refresh()
+            reset()
+        })
+    }
+
     return (
-        <div>{error.message}</div>
+        <div>
+            <p>{error.message}</p>
+            <button onClick={() => reload()}>try again</button>
+        </div>
     )
 }
