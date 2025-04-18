61d054f424d9a591aff745edab447f1970320a36
Preston Fassbender <fassbenderp0551@gmail.com>
2025-04-18 09:57:26 -0500

    handling PATCH requests

--- route-handlers-demo/src/app/comments/[id]/route.js
+++ route-handlers-demo/src/app/comments/[id]/route.js
@@ -5,3 +5,13 @@ export async function GET(_request, { params }) {
     const comment = comments.find((comment) => comment.id === parseInt(id))
     return Response.json(comment)
 }
+
+export async function PATCH(request, { params }) {
+    const { id } = await params
+    const body = await request.json()
+    const { text } = body
+
+    const index = comments.findIndex((comment) => comment.id === parseInt(id))
+    comments[index].text = text
+    return Response.json(comments[index])
+}
