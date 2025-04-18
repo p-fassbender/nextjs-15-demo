87027519caa27f30fbc28d7319cb273f1a3f3ce2
Preston Fassbender <fassbenderp0551@gmail.com>
2025-04-18 09:50:23 -0500

    dynamic route handlers

---
+++ route-handlers-demo/src/app/comments/[id]/route.js
@@ -0,0 +1,7 @@
+import { comments } from "../data";
+
+export async function GET(_request, { params }) {
+    const { id } = await params
+    const comment = comments.find((comment) => comment.id === parseInt(id))
+    return Response.json(comment)
+}
